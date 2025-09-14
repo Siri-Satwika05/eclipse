aa9c30bd1367b13a6a168f7284edf1458a23fbe3
SIRI SATWIKA POLOJU <SIRI SATWIKA POLOJU@LAPTOP-HD2AJQ0H>
2025-09-15 00:04:25 +0530

    conditions.

---
+++ TNS/src/com/tns/conditions/prime1.java
@@ -0,0 +1,18 @@
+package com.tns.conditions;
+
+public class prime1 {
+	public static void main(String args[]) {
+		for(int i=2;i<+100;i++) {
+			int flag=0;
+			for(int j=2;j<i;j++) {
+				if(i%j==0) {
+					flag=1;
+				}
+			}
+			if(flag==0) {
+				System.out.println(i);
+			}
+		}
+	}
+
+}
