d833acedc4775d0fb54102c551dccabc1d6170e4
SIRI SATWIKA POLOJU <SIRI SATWIKA POLOJU@LAPTOP-HD2AJQ0H>
2025-10-10 18:30:38 +0530

    jdbc

---
+++ jdbcprograms/src/excutor/CurdOperations.java
@@ -0,0 +1,10 @@
+package excutor;
+
+public interface CurdOperations {
+
+	static void addStudent(int i, String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ jdbcprograms/src/excutor/JDBCMain.java
@@ -0,0 +1,12 @@
+package excutor;
+
+public class JDBCMain {
+
+	public static void main(String[] args) {
+		CurdOperations.addStudent(101, "Seetha");
+		CurdOperations.addStudent(102, "Geetha");
+		CurdOperations.addStudent(103, "Ramu");
+
+	}
+
+}
